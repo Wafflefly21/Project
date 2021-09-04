720efef7f9403ce6b4a95edcae83c60efa0e7d7b
Wafflefly21 <90030102+Wafflefly21@users.noreply.github.com>
2021-09-04 15:21:14 +1000

    Update mraid.js

--- assets/js/mraid.js
+++ assets/js/mraid.js
@@ -1,10 +1,3 @@
-/*  Copyright (c) 2011 The mraid.org project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree. All contributing project authors may
- *  be found in the AUTHORS file in the root of the source tree.
- */
 (function() {
 
 	   var mraidview = window.mraidview = {};
